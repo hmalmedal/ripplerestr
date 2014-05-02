6a5bce3b2cf7162fa580bb73c1a0ca5003fc40f3
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-02 19:52:25 +0200

    Account balances

---
+++ R/balances.R
@@ -0,0 +1,11 @@
+#' Get balances
+#' 
+#' Get an account's existing balances. This includes XRP balance (which does not
+#' include a counterparty) and trustline balances.
+#'
+#' @export
+account_balances <- function(address, server = "http://localhost:5990") {
+    endpoint <- paste0("/v1/accounts/", address, "/balances")
+    url  <- paste0(server, endpoint)
+    return(content(GET(url)))
+}

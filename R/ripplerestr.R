d162a50cee74cf62c5941f6a15ab8fc0c9bd37ef
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-03 23:31:24 +0200

    Update functions and move them

--- R/ripplerestr.R
+++ R/ripplerestr.R
@@ -27,3 +27,60 @@ NULL
         stop("No output to parse", call. = FALSE)
     jsonlite::fromJSON(text, simplifyVector = FALSE)
 }
+
+#' Get server info
+#'
+#' Retrieve information about the \code{ripple-rest} and connected
+#' \code{rippled}'s current status.
+#'
+#' @export
+server_status <- function(...) {
+    path <- "v1/server"
+    .GET(path, ...)
+}
+
+#' Get connected state
+#'
+#' A simple endpoint that can be used to check if \code{ripple-rest} is
+#' connected to a \code{rippled} and is ready to serve. If used before querying
+#' the other endpoints this can be used to centralize the logic to handle if
+#' \code{rippled} is disconnected from the Ripple Network and unable to process
+#' transactions.
+#'
+#' @export
+server_connected <- function(...) {
+    path <- "v1/server/connected"
+    .GET(path, ...)
+}
+
+#' Get UUID
+#'
+#' A UUID v4 generator, which can be used if the client wishes to use UUIDs for
+#' the \code{client_resource_id} but does not have a UUID generator handy.
+#'
+#' @export
+uuid_generator <- function(...) {
+    path <- "v1/uuid"
+    .GET(path, ...)
+}
+
+#' Get balances
+#'
+#' Get an account's existing balances. This includes XRP balance (which does not
+#' include a counterparty) and trustline balances.
+#'
+#' @export
+account_balances <- function(address, ...) {
+    path <- paste0("v1/accounts/", address, "/balances")
+    .GET(path, ...)
+}
+
+#' Get trustlines
+#'
+#' Get an account's existing trustlines
+#'
+#' @export
+get_account_trustlines <- function(address, ...) {
+    path <- paste0("v1/accounts/", address, "/trustlines")
+    .GET(path, ...)
+}

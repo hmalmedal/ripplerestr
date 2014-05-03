c7c6889bc7aab5b4ca7b5034144313f70df65e86
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-03 18:20:48 +0200

    Get an account's existing trustlines

--- R/balances.R
+++ R/balances.R
@@ -9,3 +9,14 @@ account_balances <- function(address, server = "http://localhost:5990") {
     url  <- paste0(server, endpoint)
     return(content(GET(url)))
 }
+#' Get trustlines
+#' 
+#' Get an account's existing trustlines
+#'
+#' @export
+get_account_trustlines <- function(address,
+                                   server = "http://localhost:5990") {
+    endpoint <- paste0("/v1/accounts/", address, "/trustlines")
+    url  <- paste0(server, endpoint)
+    return(content(GET(url)))
+}

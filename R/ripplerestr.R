88a240a75d4726fed78f3857c2f882e891437cfd
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-04 16:27:03 +0200

    Move functions

--- R/ripplerestr.R
+++ R/ripplerestr.R
@@ -39,45 +39,6 @@ NULL
     jsonlite::fromJSON(text, simplifyVector = FALSE)
 }
 
-#' Get server info
-#'
-#' Retrieve information about the \code{ripple-rest} and connected
-#' \code{rippled}'s current status.
-#'
-#' @export
-server_status <- function(...) {
-    path <- "v1/server"
-    .GET(path, ...)
-}
-
-#' Get connected state
-#'
-#' A simple endpoint that can be used to check if \code{ripple-rest} is
-#' connected to a \code{rippled} and is ready to serve. If used before querying
-#' the other endpoints this can be used to centralize the logic to handle if
-#' \code{rippled} is disconnected from the Ripple Network and unable to process
-#' transactions.
-#'
-#' @return TRUE or FALSE
-#'
-#' @export
-server_connected <- function(...) {
-    path <- "v1/server/connected"
-    req <- .GET(path, ...)
-    .parse(req)$connected
-}
-
-#' Get UUID
-#'
-#' A UUID v4 generator, which can be used if the client wishes to use UUIDs for
-#' the \code{client_resource_id} but does not have a UUID generator handy.
-#'
-#' @export
-uuid_generator <- function(...) {
-    path <- "v1/uuid"
-    .GET(path, ...)
-}
-
 #' Get balances
 #'
 #' Get an account's existing balances. This includes XRP balance (which does not

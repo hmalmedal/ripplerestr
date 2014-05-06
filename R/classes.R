bb79e5cd6cb1c139f89c9f0a47178b129e023a8e
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-06 12:42:32 +0200

    Remove prototypes

--- R/classes.R
+++ R/classes.R
@@ -4,8 +4,7 @@
 #'
 #' @export Currency
 #' @exportClass Currency
-Currency <- setClass("Currency", contains = "character",
-                     prototype = "XRP")
+Currency <- setClass("Currency", contains = "character")
 validCurrencyObject <- function(object) {
     if (!all(grepl("^([a-zA-Z0-9]{3}|[A-Fa-f0-9]{40})$", object))) {
         return("Invalid currency.")
@@ -30,9 +29,7 @@ setValidity("Currency", validCurrencyObject)
 Balance <- setClass(Class = "Balance",
                     slots = c(value = "numeric",
                               currency = "Currency",
-                              counterparty = "character"),
-                    prototype = list(value = 0,
-                                     counterparty = ""))
+                              counterparty = "character"))
 validBalanceObject <- function(object) {
     if ((length(object@value) != length(object@currency)) ||
             (length(object@value) != length(object@counterparty))) {

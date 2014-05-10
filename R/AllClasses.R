a3ddd226ad462596d88ebdef04dc2a9cc2935c58
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-10 18:18:36 +0200

    Amount class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -128,6 +128,48 @@ validBalanceObject <- function(object) {
 }
 setValidity("Balance", validBalanceObject)
 
+#' Amount
+#'
+#' An Amount on the Ripple Protocol, used also for XRP in the ripple-rest API
+#'
+#' @slot value Object of class \code{"numeric"}. The quantity of the currency.
+#' @slot currency Object of class \code{"\link{Currency}"}. The currency
+#'   expressed as a three-character code.
+#' @slot issuer Object of class \code{"character"}. The Ripple account address
+#'   of the currency's issuer or gateway, or an empty string if the currency is
+#'   XRP.
+#' @slot counterparty Object of class \code{"character"}. The Ripple account
+#'   address of the currency's issuer or gateway, or an empty string if the
+#'   currency is XRP.
+#'
+#' @export Amount
+#' @exportClass Amount
+Amount <- setClass("Amount",
+                   slots = c(value = "numeric",
+                             currency = "Currency",
+                             issuer = "character",
+                             counterparty = "character"),
+                   prototype = list(value = NA_real_,
+                                    currency = Currency("XRP"),
+                                    issuer = "",
+                                    counterparty = ""))
+validAmountObject <- function(object) {
+    l <- sapply(slotNames(object),
+                function(slotname) length(slot(object, slotname)))
+    if (!all(l==1))
+        return("Not all lengths are 1.")
+    if (!all(grepl("^$|^r[1-9A-HJ-NP-Za-km-z]{25,33}$",
+                   object@issuer))) {
+        return("Invalid issuer.")
+    }
+    if (!all(grepl("^$|^r[1-9A-HJ-NP-Za-km-z]{25,33}$",
+                   object@counterparty))) {
+        return("Invalid counterparty.")
+    }
+    return(TRUE)
+}
+setValidity("Amount", validAmountObject)
+
 #' Notification
 #'
 #' @slot account Object of class \code{"\link{RippleAddress}"}. The Ripple

537ceae04893d349a28fef8979043b79e16d0020
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-11 12:04:02 +0200

    Move assignment inside test_that

--- tests/test-when-connected/test-notifications.R
+++ tests/test-when-connected/test-notifications.R
@@ -1,10 +1,9 @@
 library(ripplerestr)
 context("notifications")
 
-nf <- get_notification("r3PDtZSa5LiYp1Ysn1vMuMzB59RzV3W9QH",
-                       "E08D6E9754025BA2534A78707605E0601F03ACE063687A0CA1BDDACFCD1698C7")
-
 test_that("classes are correct", {
+    nf <- get_notification("r3PDtZSa5LiYp1Ysn1vMuMzB59RzV3W9QH",
+                           "E08D6E9754025BA2534A78707605E0601F03ACE063687A0CA1BDDACFCD1698C7")
     expect_that(is(nf, "Notification"), is_true())
     expect_that(is(nf@account, "RippleAddress"), is_true())
     expect_that(is(nf@type, "character"), is_true())

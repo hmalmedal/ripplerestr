3365f651a3730ebac55b28a34890b089168f9de7
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-11 23:00:50 +0200

    Combine for Balance class

---
+++ R/c-methods.R
@@ -0,0 +1,20 @@
+#' Combine for Balance class
+#'
+#' @param recursive ignored
+setMethod("c",
+          signature(x = "Balance"),
+          function (x, ..., recursive = FALSE)
+          {
+              if (nargs() == 1)
+                  x
+              else if (nargs() > 2)
+                  c(x, c(...))
+              else if (is(..., "Balance")) {
+                  x@value <- c(x@value, slot(..., "value"))
+                  x@currency <- Currency(c(x@currency, slot(..., "currency")))
+                  x@counterparty <- c(x@counterparty, slot(..., "counterparty"))
+                  x
+              } else
+                  list(x, ...)
+          }
+)

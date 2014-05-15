11bf3cfd11b70def5bed85f85b5bb971fc9dbb3e
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-15 22:38:27 +0200

    Document return value of submit_payment

--- R/payments.R
+++ R/payments.R
@@ -169,7 +169,9 @@ get_payment_paths <- function(address, destination_account,
 #'   \code{port} – passed on to \code{\link{httr}}'s \code{\link{modify_url}}.
 #'   See \code{\link{is_server_connected}} for details.
 #'
-#' @return A list
+#' @return A named list. The first element is the \code{"client_resource_id"}
+#'   you gave. The second element is named \code{"status_url"} and can be used
+#'   with \code{\link{check_payment_status}}.
 #'
 #' @export
 submit_payment <- function(payment, secret, client_resource_id, ...) {

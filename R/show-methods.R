b408b86c65e09d2333e2172cb39fbc4f61c60978
Håkon Malmedal <hmalmedal@gmail.com>
2014-05-15 21:53:46 +0200

    Show method for AccountSettings class
    * Don't show empty slots
    * Print transfer_rate as decimal

--- R/show-methods.R
+++ R/show-methods.R
@@ -32,3 +32,23 @@ setMethod("show",
                                    issuer)))
           }
 )
+#' Show for AccountSettings class
+#'
+setMethod("show",
+          signature(object = "AccountSettings"),
+          function (object)
+          {
+              cat("An object of class \"AccountSettings\"\n")
+              for (slotname in slotNames(object)) {
+                  s <- slot(object, slotname)
+                  if (length(s) > 0) {
+                      cat("Slot \"", slotname, "\":\n", sep = "")
+                      if (slotname == "transfer_rate")
+                          if (s == 0) print(1) else print(unclass(s / 1e9))
+                      else
+                          print(s)
+                      cat("\n")
+                  }
+              }
+          }
+)
